4ff3ffcb379d656aaf4db7b0a1a6904367d52676
arodriguezgim <arodriguezg@iesch.org>
2022-09-26 18:22:20 +0200

    añadido binding

--- 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/DetailActivity.java
+++ 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/DetailActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.iesch.a02_registro_de_superheroes.databinding.ActivityMainBinding;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
     }
 }
--- 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/MainActivity.java
+++ 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.iesch.a02_registro_de_superheroes.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
     }
 }
