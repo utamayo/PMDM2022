a478e408a5cce253ed8ba3ae8b3988447b90ce0f
arodriguezgim <arodriguezg@iesch.org>
2022-09-29 17:11:50 +0200

    pasa datos a segundo activity ok

--- 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/DetailActivity.java
+++ 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/DetailActivity.java
@@ -15,7 +15,31 @@ public class DetailActivity extends AppCompatActivity {
         ActivityDetailBinding binding = ActivityDetailBinding.inflate(getLayoutInflater());
         //setContentView(R.layout.activity_main);
         setContentView(binding.getRoot());
+        // 1 - Obtengo en un objeto Bundle todos los datos traídos de origen
+        Bundle extras = getIntent().getExtras();
+        // 2 - Creo las variables para obtener los valores del intent desde extras con el valor de la llave
+        String heroName = extras.getString("superhero_name");
+        String alterEgo = extras.getString("alterEgo");
+        String bio = extras.getString("bio");
+        float rating = extras.getFloat("rating");
+        // 3 - Utilizamos los valores obtenidos
+        binding.nombreHeroeText.setText(heroName);
+        binding.textView3.setText(alterEgo);
+        binding.textView4.setText(bio);
+        binding.ratingBar.setRating(rating);
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/MainActivity.java
+++ 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/MainActivity.java
@@ -18,11 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.saveButton.setOnClickListener(v -> {
-            //1 - Nos creamos un objeto de tipo Intent
-            Intent irADetalle = new Intent(this,DetailActivity.class);
-            // 2- Llamamos a startActivity para ir a DetailActivity
-            startActivity(irADetalle);
+            // 3 b - Captamos la información de las diferentes vistas
+            String superHeroName = binding.nombreHeroEdit.getText().toString();
+            String alterEgo = binding.alterEgoEdit.getText().toString();
+            String bio = binding.bioEdit.getText().toString();
+            float rating = binding.powerBar.getRating();
+
+            abrirDetailActivity(superHeroName,alterEgo,bio,rating);
+
         });
+    }
+
+    private void abrirDetailActivity( String superheroName,String alterEgo, String bio, float rating) {
+
+        //1 - Nos creamos un objeto de tipo Intent
+        Intent irADetalle = new Intent(this,DetailActivity.class);
+        // 3b - Añadimos al intent TODA LA INFORMACION que queramos pasar
+        irADetalle.putExtra("superhero_name",superheroName );
+        irADetalle.putExtra("alterEgo",alterEgo );
+        irADetalle.putExtra("bio",bio );
+        irADetalle.putExtra("rating",rating );
+
+        // 2- Llamamos a startActivity para ir a DetailActivity
+        startActivity(irADetalle);
     }
 }
 
