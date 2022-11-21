d5bd90c5dd6bfe8f4404935d1dfb75e34ef624d8
arodriguezgim <arodriguezg@iesch.org>
2022-11-21 18:30:28 +0100

    fin clase hoy

---
+++ 16_FIREBASE_Servicios/app/src/main/java/org/iesch/mi_proyecto_firebase_dam/MainActivity.java
@@ -0,0 +1,14 @@
+package org.iesch.mi_proyecto_firebase_dam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
