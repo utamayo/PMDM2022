96d50a0d108d54e0ef5ddb68333024535171a0df
arodriguezgim <arodriguezg@iesch.org>
2023-01-16 16:48:04 +0100

    Optimizacion de codigo

--- Flutter/flutter_03_app_postnavidena/lib/screens/contador_screen.dart
+++ Flutter/flutter_03_app_postnavidena/lib/screens/contador_screen.dart
@@ -8,8 +8,24 @@ class ContadorScreen extends StatefulWidget {
 }
 
 class _ContadorScreenState extends State<ContadorScreen> {
-  // variables o propiedades
-  int contador = 9;
+  int contador = 10;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+    print(contador);
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void resetear() {
+    contador = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const fontsize30 = TextStyle(fontSize: 30);
@@ -30,14 +46,49 @@ class _ContadorScreenState extends State<ContadorScreen> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          contador++;
-          setState(() {});
-        },
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: BotoneraPersonalizada(
+        incrementarFn: incrementar,
+        decrementarFn: decrementar,
+        resetearFn: resetear,
       ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+    );
+  }
+}
+
+class BotoneraPersonalizada extends StatelessWidget {
+  final Function incrementarFn;
+  final Function decrementarFn;
+  final Function resetearFn;
+
+  const BotoneraPersonalizada({
+    Key? key,
+    required this.incrementarFn,
+    required this.decrementarFn,
+    required this.resetearFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => decrementarFn,
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_outlined),
+          onPressed: () => resetearFn,
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            print('Llamo a incrementar');
+            incrementarFn;
+          },
+        ),
+      ],
     );
   }
 }
