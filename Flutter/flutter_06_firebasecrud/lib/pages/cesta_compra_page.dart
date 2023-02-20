82f77fccd97f0e7818c4b7a62838aa05682f908a
arodriguezgim <arodriguezg@iesch.org>
2023-02-20 17:07:12 +0100

    CRD terminado

--- Flutter/flutter_06_firebasecrud/lib/pages/cesta_compra_page.dart
+++ Flutter/flutter_06_firebasecrud/lib/pages/cesta_compra_page.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_06_firebasecrud/model/producto.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+const COLLECTION_NAME = 'cesta_compra';
 
 class CestaCompraPage extends StatefulWidget {
   const CestaCompraPage({super.key});
@@ -15,7 +18,7 @@ class _CestaCompraPageState extends State<CestaCompraPage> {
   @override
   void initState() {
     super.initState();
-    FirebaseFirestore.instance.collection('cesta_compra').snapshots().listen(
+    FirebaseFirestore.instance.collection(COLLECTION_NAME).snapshots().listen(
       (productos) {
         mapProductos(productos);
       },
@@ -26,7 +29,7 @@ class _CestaCompraPageState extends State<CestaCompraPage> {
   // Consultamos los registros de la coleccion de Firebase Firestorage
   void fetchProductos() async {
     var lista_productos =
-        await FirebaseFirestore.instance.collection('cesta_compra').get();
+        await FirebaseFirestore.instance.collection(COLLECTION_NAME).get();
     mapProductos(lista_productos);
   }
 
@@ -50,16 +53,94 @@ class _CestaCompraPageState extends State<CestaCompraPage> {
       appBar: AppBar(
         title: const Text('Cesta Compra'),
         elevation: 0,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                //Abro el Dialogo para introducir los datos
+                showItemDialog();
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       body: ListView.builder(
           itemCount: listaProductos.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(listaProductos[index].descripcion),
-              subtitle: Text(listaProductos[index].cantidad ?? ''),
+            return Slidable(
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  // A SlidableAction can have an icon and/or a label.
+                  SlidableAction(
+                    onPressed: (c) {
+                      eliminarProducto(listaProductos[index].id);
+                    },
+                    backgroundColor: Color(0xFFFE4A49),
+                    foregroundColor: Colors.white,
+                    icon: Icons.delete,
+                    label: 'Delete',
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: Text(listaProductos[index].descripcion),
+                subtitle: Text(listaProductos[index].cantidad ?? ''),
+              ),
             );
           }),
     );
+  }
+
+  void showItemDialog() {
+    // 2 - Los EditController necesitan sus controladores asociados
+    var nameController = TextEditingController();
+    var quantityController = TextEditingController();
+    // 1 - Llamamos a showDialog para abrir una ventana emergente
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Añadir Producto'),
+                TextField(
+                  decoration: const InputDecoration(
+                      labelText: 'Descripción del producto'),
+                  controller: nameController,
+                ),
+                TextField(
+                  decoration: const InputDecoration(labelText: 'Cantidad'),
+                  controller: quantityController,
+                ),
+                TextButton(
+                    onPressed: () {
+                      var descripcion = nameController.text.trim();
+                      var cantidad = quantityController.text.trim();
+                      addProducto(descripcion, cantidad);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Añadir')),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void addProducto(String descripcion, String cantidad) {
+    Producto producto =
+        Producto(id: 'id', descripcion: descripcion, cantidad: cantidad);
+    // Subo la instancia del producto a Firebase
+    FirebaseFirestore.instance
+        .collection(COLLECTION_NAME)
+        .add(producto.toJson());
+  }
+
+  void eliminarProducto(String id) {
+    FirebaseFirestore.instance.collection(COLLECTION_NAME).doc(id).delete();
   }
 }
