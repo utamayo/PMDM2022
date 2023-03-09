0474630a38e66b39d7b3a6bf3a4e46040abb9f55
arodriguezgim <arodriguezg@iesch.org>
2023-03-09 16:53:45 +0100

    loginPage

--- Flutter/flutter_11_firebaseauth/lib/pages/login_page.dart
+++ Flutter/flutter_11_firebaseauth/lib/pages/login_page.dart
@@ -1,43 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_09_firebaseauth/pages/estudiante/home_estudiante.dart';
+import 'package:flutter_09_firebaseauth/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 40,
-                child: TextField(
-                  //controller: controller,
-                  decoration: InputDecoration(prefixIcon: Icon(Icons.email)),
+    //final provider = Provider.of<AuthProvider>(context);
+    return Consumer<AuthProvider>(builder: (context, model, _) {
+      return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Scaffold(
+              body: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 40,
+                        child: TextField(
+                          //controller: controller,
+                          decoration:
+                              InputDecoration(prefixIcon: Icon(Icons.email)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                        child: TextField(
+                          //controller: controller,
+                          decoration:
+                              InputDecoration(prefixIcon: Icon(Icons.password)),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: null,
+                        child: Text('Entrar'),
+                      ),
+                      TextButton(
+                        onPressed: null,
+                        child: Text('Registrarse'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 40,
-                child: TextField(
-                  //controller: controller,
-                  decoration: InputDecoration(prefixIcon: Icon(Icons.password)),
-                ),
-              ),
-              TextButton(
-                onPressed: null,
-                child: Text('Entrar'),
-              ),
-              TextButton(
-                onPressed: null,
-                child: Text('Registrarse'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          } else {
+            return const HomeEstudiantePage();
+          }
+        },
+      );
+    });
   }
 }
